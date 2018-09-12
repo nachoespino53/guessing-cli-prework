9137eeabd28b13d7f05c4819e80367cfb2b22c9c
Ignacio R Espino <irespino@capozios.com>
2018-09-12 16:00:29 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,5 +9,14 @@ end
 
 
 run_guessing_game
+puts "Enter a number 1 to 100"
+
+loop do
+  puts "Enter a number 1 to 100"
+  input = gets.chomp
+  number = number_generator
+  
+  if input == 
+end
 
 end
