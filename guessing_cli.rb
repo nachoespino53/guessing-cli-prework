5da5032c03951ac3afeaa4782da4774ec76efce7
Ignacio R Espino <irespino@capozios.com>
2018-09-12 16:08:34 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,31 +1,31 @@
-number_generator
-  rand(100)
-end
+# number_generator
+#   rand(100)
+# end
 
 
-run_guessing_game
-  puts "Enter a number 1 to 100"
+# run_guessing_game
+#   puts "Enter a number 1 to 100"
   
-  loop do
-    puts "Enter a number 1 to 100"
-    input = gets.chomp
-    number = number_generator
+#   loop do
+#     puts "Enter a number 1 to 100"
+#     input = gets.chomp
+#     number = number_generator
     
-    if input.to_i > 0 && input.to_i < 101
+#     if input.to_i > 0 && input.to_i < 101
       
-      if input = number
-        puts "You guessed the correct number!"
-      else
-        puts "The computer guessed #{number}."
-      end
+#       if input = number
+#         puts "You guessed the correct number!"
+#       else
+#         puts "The computer guessed #{number}."
+#       end
       
-    elsif input == "exit" 
-      break
-    else
-      puts "Please enter a valid number"
-    end
+#     elsif input == "exit" 
+#       break
+#     else
+#       puts "Please enter a valid number"
+#     end
     
-    puts "Goodbye"
-  end
+#     puts "Goodbye"
+#   end
 
-end
+# end
